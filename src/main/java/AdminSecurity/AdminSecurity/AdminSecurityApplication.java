b2bd947c583cb26f1e2ef8a1e82280182c045d10
YOOBINNOH <kuninyb@naver.com>
2023-03-22 13:58:23 +0900

    first commit

---
+++ src/main/java/AdminSecurity/AdminSecurity/AdminSecurityApplication.java
@@ -0,0 +1,13 @@
+package AdminSecurity.AdminSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AdminSecurity/AdminSecurity/AdminSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package AdminSecurity.AdminSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
