5feecfe577bab08a2c633b96851373680b0147cd
YOOBINNOH <kuninyb@naver.com>
2023-03-22 16:02:44 +0900

    feat : Practice OAuth with github

--- src/main/java/AdminSecurity/AdminSecurity/WebSecurityConfig.java
+++ src/main/java/AdminSecurity/AdminSecurity/WebSecurityConfig.java
@@ -17,15 +17,10 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin((form) -> form
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .logout((logout) -> logout.permitAll());
+                .oauth2Login()
+                .authorizationEndpoint()
+                .baseUri("/login");
+
 
         return http.build();
     }
